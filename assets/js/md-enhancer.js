960369782e14fb5bb1c356a78756dbc3f245b1b9
Q. Chen <qchen2015@hotmail.com>
2025-04-18 13:58:14 +0800

    fix insertion

--- assets/js/md-enhancer.js
+++ assets/js/md-enhancer.js
@@ -1,52 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Header template
+    // Header template with pure HTML
     const headerTemplate = `
 <div align="center">
+    <div style="margin-bottom: 20px;">
+        <a href="https://kotlinlang.org"><img src="https://img.shields.io/badge/Kotlin-2.0.10-blue.svg" alt="Kotlin"></a>
+        <a href="https://gradle.org"><img src="https://img.shields.io/badge/Gradle-8.10.2-green.svg" alt="Gradle"></a>
+        <a href="https://www.oracle.com/java/technologies/downloads/"><img src="https://img.shields.io/badge/JDK-21.0.6-orange.svg" alt="JDK"></a>
+        <a href="LICENSE"><img src="https://img.shields.io/badge/License-MIT-yellow.svg" alt="License"></a>
+    </div>
 
-[![Kotlin](https://img.shields.io/badge/Kotlin-2.0.10-blue.svg)](https://kotlinlang.org)
-[![Gradle](https://img.shields.io/badge/Gradle-8.10.2-green.svg)](https://gradle.org)
-[![JDK](https://img.shields.io/badge/JDK-21.0.6-orange.svg)](https://www.oracle.com/java/technologies/downloads/)
-[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE)
+    <p>A collection of Kotlin projects demonstrating various Kotlin development concepts and practices.</p>
 
-A collection of Kotlin projects demonstrating various Kotlin development concepts and practices.
-
-[Repository](https://github.com/qchen-fdii-cardc/kotlin-projects) | [Author](https://github.com/qchen-fdii-cardc/)
-
+    <p>
+        <a href="https://github.com/qchen-fdii-cardc/kotlin-projects">Repository</a> | 
+        <a href="https://github.com/qchen-fdii-cardc/">Author</a>
+    </p>
 </div>
 
----
-
+<hr>
 `;
 
-    // Footer template
+    // Footer template with pure HTML
     const footerTemplate = `
-
----
-
-<div align="center">
 <hr>
 
-<a href="https://github.com/qchen-fdii-cardc/">
-  <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="Author" width="40" height="40" style="border-radius: 50%;"/>
-</a>
+<div align="center">
+    <a href="https://github.com/qchen-fdii-cardc/">
+        <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="Author" width="40" height="40" style="border-radius: 50%;"/>
+    </a>
 
-**Qin Chen**  
-**Developer**
+    <p><strong>Qin Chen</strong><br>
+    <strong>Developer</strong></p>
 
-[![GitHub](https://img.shields.io/badge/GitHub-qchen--fdii--cardc-181717?style=flat&logo=github)](https://github.com/qchen-fdii-cardc/)
-
+    <a href="https://github.com/qchen-fdii-cardc/">
+        <img src="https://img.shields.io/badge/GitHub-qchen--fdii--cardc-181717?style=flat&logo=github" alt="GitHub">
+    </a>
 </div>
 `;
 
-    // Get the main content
-    const mainContent = document.querySelector('h2');
-    if (mainContent) {
-        // Add header
-        mainContent.insertAdjacentHTML('beforebegin', headerTemplate);
-    }
-    const footerContent = document.querySelectorAll('p').last();
-    if (footerContent) {
-        // Add footer
-        footerContent.insertAdjacentHTML('afterend', footerTemplate);
-    }
+    // Insert header before the first element in body
+    document.body.insertAdjacentHTML('afterbegin', headerTemplate);
+
+    // Insert footer after the last element in body
+    document.body.insertAdjacentHTML('beforeend', footerTemplate);
 }); 
